d11616d4f6e031c3637e3f6f6e9bb161dcaa1ad1
xsekox64 <xsekox64@hotmail.com>
2023-11-11 19:29:13 +0300

    Add project files.

---
+++ AcikArrtirmaServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AcikArrtirmaServer.Domain;
+using AcikArrtirmaServer.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AcikArrtirmaServer.Infrastructure.Context;
+
+internal sealed class ApplicationDbContext : DbContext, IDbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost,1433; Initial Catalog=AcikArttirmaDb;  User ID=sa; Password=200Ser2581.,*; Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
